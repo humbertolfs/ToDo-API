6e1300ada8d3628264476747a69be1fd1023ff83
humbertolfs <humbertobz26@gmail.com>
2024-08-14 15:40:06 -0300

    Initial commit

---
+++ src/main/java/me/learning/ToDo_API/ToDoApiApplication.java
@@ -0,0 +1,13 @@
+package me.learning.ToDo_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/learning/ToDo_API/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.learning.ToDo_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
