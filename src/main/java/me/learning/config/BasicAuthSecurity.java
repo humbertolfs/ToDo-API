7696cad710040adf39cd57dfd18f7de15fe72803
humbertolfs <humbertobz26@gmail.com>
2024-08-19 16:37:44 -0300

    :sparkles: feat: Basic Authentication

--- src/main/java/me/learning/config/BasicAuthSecurity.java
+++ src/main/java/me/learning/config/BasicAuthSecurity.java
@@ -1,38 +1,39 @@
 package me.learning.config;
 
+import me.learning.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class BasicAuthSecurity {
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
-
-        http.httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
-
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AntPathRequestMatcher
+                                .antMatcher("/h2-console/**")).permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .httpBasic(Customizer
+                        .withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
         return http.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth,PasswordEncoder passwordEncoder) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(PasswordEncoderConfig.passwordEncoder())
-                .withUser("user")
-                .password(PasswordEncoderConfig.passwordEncoder().encode("password"))
-                .roles("ADMIN");
     }
 
 }
